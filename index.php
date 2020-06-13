82b99f4658aa0e3e72954d71a80de51c8d81a367
11kush11410 <66750934+11kush11410@users.noreply.github.com>
2020-06-13 16:54:15 +0530

    commit

---
+++ index.php
@@ -0,0 +1,15 @@
+echo ' <marquee> WECOME TO MY CLOUD WORLD </marquee> ';
+<br />
+<pre>
+<?php
+echo' 
+<html>
+<center> <img src="https://github.com/11kush11410/kushmania.git"  width="300" height="400">  </center>
+</html>
+';
+echo'
+<center> <h2> Terraform </h2> </center>
+     <p> Use Infrastructure as Code to provision and manage any cloud, infrastructure or service </p>
+';
+?>
+</pre>
